6ccd0c0d1795051ae8aa619cd1531d4dd4ff03d0
Foysal Mahmood <foysalcse033@gmail.com>
2026-06-04 13:47:55 +0600

    set time out

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -30,17 +30,23 @@ export default function DashboardLayout({ children }) {
       (!storedTheme &&
         window.matchMedia("(prefers-color-scheme: dark)").matches)
     ) {
-      setIsDark(true);
+      setTimeout(() => {
+        setIsDark(true);
+      }, 0);
       document.documentElement.classList.add("dark");
     } else {
-      setIsDark(false);
+      setTimeout(() => {
+        setIsDark(false);
+      }, 0);
       document.documentElement.classList.remove("dark");
     }
   }, []);
 
   // Close mobile sidebar on route change
   useEffect(() => {
-    setIsMobileSidebarOpen(false);
+    setTimeout(() => {
+      setIsMobileSidebarOpen(false);
+    }, 0);
   }, [pathname]);
 
   const toggleTheme = () => {
@@ -71,27 +77,27 @@ export default function DashboardLayout({ children }) {
       name: "Overview",
       path: "/dashboard",
       icon: "M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z",
-      show: true
+      show: true,
     },
     {
       name: "Manage Users",
       path: "/dashboard/admin",
       icon: "M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z",
-      show: !isTeamMember
+      show: !isTeamMember,
     },
     {
       name: "Project Management",
       path: "/dashboard/projects",
       icon: "M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z",
-      show: !isTeamMember
+      show: !isTeamMember,
     },
     {
       name: "My Tasks",
       path: "/dashboard/tasks",
       icon: "M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2",
-      show: true
+      show: true,
     },
-  ].filter(item => item.show);
+  ].filter((item) => item.show);
 
   return (
     <ProtectedRoute>
